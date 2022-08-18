5a65d1d14f5a15fa274de43a8c967af0a1fc2e45
Vikash Anand <vikashanand.sasi@gmail.com>
2022-08-18 15:25:23 +0530

    Updates

---
+++ precache-manifest.a30c039f21ae803f059c285e437987e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d06e8b94e5ab67dac53",
+    "url": "/xo/static/css/main.8adfc267.chunk.css"
+  },
+  {
+    "revision": "9d06e8b94e5ab67dac53",
+    "url": "/xo/static/js/main.9d06e8b9.chunk.js"
+  },
+  {
+    "revision": "48dc1c1c87d8faf3dbc4",
+    "url": "/xo/static/js/1.48dc1c1c.chunk.js"
+  },
+  {
+    "revision": "d15a28e4939725087320",
+    "url": "/xo/static/js/runtime~main.d15a28e4.js"
+  },
+  {
+    "revision": "823a61deee0a2b561d692cd44b544295",
+    "url": "/xo/index.html"
+  }
+];
